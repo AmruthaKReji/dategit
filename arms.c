8d6006744447f5f85dd8389c409c2cc922ce8935
Amrutha K Reji <amruthakreji@gmail.com>
2022-10-30 23:05:38 +0530

    This is second commit after adding div function

--- arms.c
+++ arms.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+int div(int a, int b);
 int main()
 {
 int n,sum=0,rem,o,c=0,p,r;
@@ -24,4 +25,10 @@ printf("%d is an Armstrong number\n",o);
 else
 printf("%d is not an Armstrong number\n",o);
 return 0;
+}
+int div(int a, int b)
+{
+int c,a=20,b=5;
+c=div(a,b);
+printf("result is %d\n",c); 
 }
